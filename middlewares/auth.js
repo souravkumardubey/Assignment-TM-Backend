27d24270883c6082e5a20e259d7f4ae7047fb8f7
souravkumardubey <skdlp2000@gmail.com>
2023-10-01 19:44:12 +0530

    auth middleware added

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.TOKEN_SECRET;
+
+const auth = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = auth;
